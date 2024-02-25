91a5e389c3af9d2ff327373aecd07f5b303a4b6c
PopovaOlha <fc.odess.10@gmail.com>
2024-02-25 14:10:47 +0200

    feat: add code to start HTTP and WebSocket servers

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { HTTP_PORT } from './config/index.js';
+import { color } from './console-styling/consoleStyling.js';
+import { httpServer } from './servers/httpServer.js';
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(color.magenta('⊙', 'Http server is running on'), `http://localhost:${HTTP_PORT}`);
+    console.log(color.magenta('⊙', 'Websocket server is running on'), `ws://localhost:${HTTP_PORT}`);
+  });
